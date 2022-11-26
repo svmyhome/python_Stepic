a293b044b39f9354eb6428c980bb415c473dd21d
Vladimir Sarychev <svmyhome@mail.ru>
2022-11-26 13:13:41 +0300

    Added list, tuple, list comprehension

---
+++ stepic/digits.py
@@ -0,0 +1,7 @@
+print(2 + 3)
+print(4 / 2)
+print(1000000)
+print(1_000_000)
+
+THIS_IS_CONST = 1000
+print(THIS_IS_CONST)
---
+++ stepic/string.py
@@ -0,0 +1,18 @@
+sentence = "kdshjcj cdkscknm cdsnkcmkds"
+print(sentence.title())
+print(sentence.upper())
+print(sentence.lower())
+print(sentence.capitalize())
+first_name = "ADA"
+last_name = "LoVelaS"
+FIO = f"{first_name} {last_name}"
+message = f"Hello, {FIO.title()}"
+print(message)
+print('Hello')
+print('\tHello')
+print('jjgfhjghfj dkfdfjkd')
+print('jjgfhjghfj \ndkfdfjkd')
+print(' stip ', 'strip')
+print(' stip '.rstrip(), 'strip')
+print(' stip '.lstrip(), 'strip')
+print(' stip '.strip(), 'strip')
