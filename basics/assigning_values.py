1a1df46cd0471b2168a3cbd45df917a4f423eeef
vladimir <svmyhome@mail.ru>
2024-02-04 13:47:44 +0300

    Modify string hadling

--- basics/assigning_values.py
+++ basics/assigning_values.py
@@ -8,3 +8,20 @@ print(f'C and D are references to the different object C = {id(C)}, D = {id(D)}'
 
 C, D = D, C
 print(f'The references changed C = {id(C)}, D = {id(D)}')
+
+
+print(2 + 3)
+print(4 / 2)
+print(1000000)
+print(1_000_000)
+
+THIS_IS_CONST = 1000
+print(THIS_IS_CONST)
+
+
+
+
+# input values
+
+s1, s2 = map(str.strip, input().split())
+print(s1, s2)
